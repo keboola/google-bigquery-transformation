db30cd64c73c2a2ae7da79c5d42a4eea2b4a41f1
Výborný Adam <mail@adamvyborny.cz>
2022-12-06 18:28:16 +0100

    Component configuration

--- src/Config.php
+++ src/Config.php
@@ -4,13 +4,30 @@ declare(strict_types=1);
 
 namespace BigQueryTransformation;
 
+use BigQueryTransformation\Exception\ApplicationException;
+use InvalidArgumentException;
 use Keboola\Component\Config\BaseConfig;
 
 class Config extends BaseConfig
 {
-    // @todo implement your custom getters
-    public function getFoo(): string
+    /**
+     * @return array<array{name: string, codes: array<array{name: string, script: string}>}>
+     */
+    public function getBlocks(): array
     {
-        return $this->getStringValue(['parameters', 'foo']);
+        return $this->getArrayValue(['parameters', 'blocks']);
+    }
+
+    /**
+     * @return array<string, mixed>
+     * @throws \BigQueryTransformation\Exception\ApplicationException
+     */
+    public function getDatabaseConfig(): array
+    {
+        try {
+            return $this->getArrayValue(['authorization', 'workspace']);
+        } catch (InvalidArgumentException) {
+            throw new ApplicationException('Missing authorization for workspace');
+        }
     }
 }
