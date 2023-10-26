e8b604d6949717a9f76613ed6502880731ab7663
zajca <zajca@zajca.cz>
2023-10-26 16:07:16 +0200

    increase retry count to 30

--- src/BigQueryConnection.php
+++ src/BigQueryConnection.php
@@ -99,7 +99,7 @@ class BigQueryConnection
                     return false;
                 };
             },
-            'retries' => 20,
+            'retries' => 30,
         ]);
         $this->session = (new SessionFactory($this->client))->createSession();
         /** @var string $schema */
