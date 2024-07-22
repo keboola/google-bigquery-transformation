446d6d4cff15941a384306bd6e3f30b6dcf447fe
Jiří Novák <jiri.novak@keboola.com>
2024-07-22 14:37:08 +0200

    PST-1792 add more data types for authoritative data type test

--- tests/functional/run-action-authoritative-data-type/checkResult.php
+++ tests/functional/run-action-authoritative-data-type/checkResult.php
@@ -10,9 +10,9 @@ return static function (DatadirTest $test): void {
     $result = $test->connection->executeQuery('SELECT * FROM `example` ORDER BY name;');
     assertSame(
         [
-            ['name' => 'test example name', 'usercity' => 'Prague'],
-            ['name' => 'test example name 2', 'usercity' => 'Brno'],
-            ['name' => 'test example name 3', 'usercity' => 'Ostrava'],
+            ['name' => 'test example name', 'usercity' => 'Prague', 'population' => 1380000, 'capitalcity' => true],
+            ['name' => 'test example name 2', 'usercity' => 'Brno', 'population' => 380000, 'capitalcity' => false],
+            ['name' => 'test example name 3', 'usercity' => 'Ostrava', 'population' => 280000, 'capitalcity' => false],
         ],
         iterator_to_array($result)
     );
